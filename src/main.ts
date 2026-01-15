6584f2c1a930c1e464059aec60869a1ab3d65a9d
walid-hud <walidthehud@gmail.com>
2026-01-15 12:54:23 +0100

    added demands table

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ type form_data = {
     date:string
 }
 const requests : form_data[] = []
-
+const table_body = $("#table-body")
 
 const form = $<HTMLFormElement>("#form")
 const last_name = $<HTMLInputElement>("#nom")
@@ -18,9 +18,44 @@ const tel = $<HTMLInputElement>("#telephone")
 const reason = $<HTMLInputElement>("#motif")
 const date = $<HTMLInputElement>("#date")
 const submit_handler = (ev:SubmitEvent)=>{
-    const data : form_data = {
-        fn:validate(first_name) 
+    ev.preventDefault()
+    if(!validate(first_name)){
+        first_name.setCustomValidity("prenom invalid") 
+        return
     }
+    if(!validate(last_name)){
+        last_name.setCustomValidity("Nom invalid") 
+        return
+    }
+    if(!validate(email)){
+        email.setCustomValidity("email invalid") 
+        return
+    }
+    if(!validate(tel)){
+        tel.setCustomValidity("telephone invalid") 
+        return
+    }
+    if(!validate(reason)){
+        reason.setCustomValidity("motif invalid") 
+        return
+    }
+    if(!validate(date)){
+        date.setCustomValidity("date invalid") 
+        return
+    }
+
+    const data : form_data = {
+        fn:first_name.value,
+        ln:last_name.value,
+        email:email.value,
+        tel:Number(tel.value),
+        date:date.value,
+        reason:reason.value
+    }
+
+    requests.push(data)
+    add_request(data)
+    form.reset()
 }
 
 
@@ -32,3 +67,38 @@ function validate(field:HTMLInputElement):boolean{
     return true
 }
 form.addEventListener("submit" , submit_handler)
+
+
+
+function add_request(data:form_data){
+    const row = document.createElement("tr");
+    row.innerHTML =
+`
+<tr class="">
+    <th scope="row" class="px-6 py-4 font-medium text-heading whitespace-nowrap">
+        ${data.fn} ${data.ln}
+    </th>
+    <td class="px-6 py-4">
+      ${data.reason}
+    </td>
+    <td class="px-6 py-4">
+      ${data.date}
+    </td>
+    <td class="px-6 py-4">
+      ${data.email}
+    </td>
+    <td class="px-6 py-4 flex items-center">
+        <button class=" flex w-12 px-4 cursor-pointer" title="delete entry">
+            <svg class="hover:scale-110 transition-transform duration-200 ease-in" 
+            width="11" height="12" viewBox="0 0 11 12" fill="#c62828" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M2 12C1.63334 12 1.31956 11.8696 1.05867 11.6087C0.79778 11.3478 0.667113 11.0338 0.666669 10.6667V2C0.47778 2 0.319558 1.936 0.192002 1.808C0.0644468 1.68 0.000446743 1.52178 2.29885e-06 1.33333C-0.000442146 1.14489 0.0635579 0.986667 0.192002 0.858667C0.320447 0.730667 0.478669 0.666667 0.666669 0.666667H3.33334C3.33334 0.477778 3.39734 0.319556 3.52534 0.192C3.65334 0.0644445 3.81156 0.000444444 4 0H6.66667C6.85556 0 7.014 0.0640001 7.142 0.192C7.27 0.32 7.33378 0.478222 7.33334 0.666667H10C10.1889 0.666667 10.3473 0.730667 10.4753 0.858667C10.6033 0.986667 10.6671 1.14489 10.6667 1.33333C10.6662 1.52178 10.6022 1.68022 10.4747 1.80867C10.3471 1.93711 10.1889 2.00089 10 2V10.6667C10 11.0333 9.86956 11.3473 9.60867 11.6087C9.34778 11.87 9.03378 12.0004 8.66667 12H2ZM4 9.33333C4.18889 9.33333 4.34734 9.26933 4.47534 9.14133C4.60334 9.01333 4.66711 8.85511 4.66667 8.66667V4C4.66667 3.81111 4.60267 3.65289 4.47467 3.52533C4.34667 3.39778 4.18845 3.33378 4 3.33333C3.81156 3.33289 3.65334 3.39689 3.52534 3.52533C3.39734 3.65378 3.33334 3.812 3.33334 4V8.66667C3.33334 8.85556 3.39734 9.014 3.52534 9.142C3.65334 9.27 3.81156 9.33378 4 9.33333ZM6.66667 9.33333C6.85556 9.33333 7.014 9.26933 7.142 9.14133C7.27 9.01333 7.33378 8.85511 7.33334 8.66667V4C7.33334 3.81111 7.26934 3.65289 7.14134 3.52533C7.01334 3.39778 6.85511 3.33378 6.66667 3.33333C6.47822 3.33289 6.32 3.39689 6.192 3.52533C6.064 3.65378 6 3.812 6 4V8.66667C6 8.85556 6.064 9.014 6.192 9.142C6.32 9.27 6.47822 9.33378 6.66667 9.33333Z"
+                fill="#c62828" />
+            </svg>
+        </button>
+    </td>
+</tr>
+`
+
+table_body.appendChild(row.cloneNode(true))
+}
